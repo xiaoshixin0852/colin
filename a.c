c07543ef6d73736bc6ec13bfefcba24948e25efe
xiaoshixin0852 <3497854609@qq.com>
2025-11-28 18:38:23 +0800

    change upload

--- a.c
+++ a.c
@@ -1,6 +1,16 @@
 #include<stdio.h>
 int main(void)
 {
-	printf("testing..\n");
-	return 0;
+	printf("1testing..\n");
+	printf("2testing..\n");
+	printf("3testing..\n");
+	printf("4testing..\n");
+	printf("5testing..\n");
+	printf("6testing..\n");
+
+
+
+
+
+
 }
